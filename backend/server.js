beab30e8703a0fb66b84c530a52ea953b7ee4e47
rakeshbhat02 <rakeshaedurkala@gmail.com>
2024-12-30 16:37:55 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,22 @@ import userRoutes from "./routes/user.route.js";
 import messageRoutes from './routes/message.route.js';
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import {app, server} from './socket/socket.js';
+import path from 'path';
 
 app.use(express.json());
 app.use(cookieParser());
 
 const PORT = process.env.PORT;
-// app.get("/", (req, res) => {
-//   res.send("Hello world");
-// });
+const __dirname = path.resolve();
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
- 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
